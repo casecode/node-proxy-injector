1b01b27e19acc3fdd94aea6e335651820490856c
Casey White <casey.r.white@gmail.com>
2015-01-13 00:16:36 -0800

    reorganize injector module to accept config options and scripts inputs

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var http = require('http');
 var connect = require('connect');
 var proxyInjector = require('./lib/proxy-injector');
 
-var app = connect();
+var options = {
+  url: {
+    hostname: '63rdstreetproductions.com',
+    path: '/about'
+  }
+};
 
-var onRequest = proxyInjector();
-app.use(onRequest);
+var scripts = {
+  js: [
+    'alert("IT WORKED");'
+  ]
+}
+
+var proxy = proxyInjector.createProxyServer(options, scripts);
+
+var app = connect();
+app.use(proxy);
 
 http.createServer(app).listen(8000);
