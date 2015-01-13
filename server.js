c4d9349357eaa7761af4a93eaa3327c588e68607
Casey White <casey.r.white@gmail.com>
2015-01-12 21:14:14 -0800

    add server.js with basic proxy and injection setup

---
+++ server.js
@@ -0,0 +1,47 @@
+var http = require('http');
+var cheerio = require('cheerio');
+
+http.createServer(onRequest).listen(8000);
+
+var injectionScripts = {
+  js: [
+  'alert("IT WORKED");'
+  ]
+}
+
+function onRequest(clientReq, clientRes) {
+  // console.log('serve: ' + clientReq.url);
+
+  var options = {
+    hostname: '63rdstreetproductions.com',
+    port: 80,
+    path: clientReq.url,
+    method: 'GET'
+  };
+
+  var proxy = http.request(options, function (res) {
+    if (clientReq.url === '/') {
+      var htmlData;
+      res.on('data', function(chunk) {
+        htmlData += chunk;
+      });
+      res.on('end', function() {
+        htmlData = htmlData.replace('undefined', '');
+        var $ = cheerio.load(htmlData);
+        injectionScripts.js.forEach(function(script) {
+          $('head').append('<script type="text/javascript">' + script + '</script>');
+        });
+        var html = $.html();
+        clientRes.write(html);
+      });
+    } else {
+      res.pipe(clientRes, {
+        end: true
+      });
+    }
+  });
+
+  clientReq.pipe(proxy, {
+    end: true
+  });
+}
