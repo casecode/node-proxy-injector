a190fb8239c86b8ff25f95ea779012650d82fbea
Casey White <casey.r.white@gmail.com>
2015-01-14 13:25:12 -0800

    add command line options to specifiy url and target dir

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
+var program = require('commander');
 var http = require('http');
+var url = require('url');
 var path = require('path');
 var connect = require('connect');
 var proxyInjector = require('./lib/proxy-injector');
 var livereload = require('livereload');
 
 
+program
+  .version('0.0.1')
+  .usage('[options] <file ...>')
+  .option('-u, --target-url [url]', 'The target url to proxy', 'http://jquery.com/')
+  .option('-d, --target-dir [path]', 'The path of the target directory to watch', './test')
+  .parse(process.argv);
+
+// resolve options args
+var targetUrl = program.targetUrl;
+if (!(targetUrl.substring(0, 4) === "http")) {
+  targetUrl = 'http://' + targetUrl
+}
+targetUrl = url.parse(targetUrl);
+
+var targetDir = path.resolve(__dirname, program.targetDir);
+
+
 // Config options
 var options = {
-  // url to be proxied
-  targetUrl: {
-    hostname: '63rdstreetproductions.com',
-    path: '/about'
-  },
-  // local proxy server port
-  proxyPort: 8000,
-  // directory containing scripts and stylesheets for injection
-  targetDir: path.resolve(__dirname, './test')
+  targetUrl: targetUrl,
+  proxyPort: 8000, // local proxy server port
+  targetDir: targetDir // directory containing scripts and stylesheets for injection
 };
 
 var proxy = proxyInjector.createProxyServer(options);
